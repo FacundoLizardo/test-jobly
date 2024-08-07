bed85bd079bef35944eeef5d78bfcdc3bb35f6b4
FacundoLizardo <facundolizardo75@gmail.com>
2024-08-07 03:12:15 -0300

    Set up server with CORS
    - Configured CORS middleware to handle cross-origin requests.
    - Added custom headers for CORS to allow requests from any origin.
    - Ensured server is capable of handling various HTTP methods and headers.

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './db';
 import newsRoutes from './Routes/news'
 import favoritesRoutes from "./Routes/favorites";
+const cors = require("cors")
 
 dotenv.config();
 
@@ -12,7 +13,18 @@ const PORT = process.env.PORT;
 connectDB();
 
 app.use(express.json());
+app.use(cors())
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT);
 });
