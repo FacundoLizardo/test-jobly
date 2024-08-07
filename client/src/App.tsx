bd32e847a01be753b27e05ec94e8a7d08ddff847
FacundoLizardo <facundolizardo75@gmail.com>
2024-08-07 20:34:45 -0300

    Initialize React project with React Router
    - Created a new React project.
    - Installed React Router for client-side routing.
    - Set up basic routing structure.
    - Configured main routes and navigation.

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./App.css";
+import Search from "@/components/Search.tsx";
+import AppRoutes from "@/Routes.tsx";
+import Navigation from "@/components/Navigation.tsx";
+
+function App() {
+  return <AppRoutes />;
+}
+
+export default App;
