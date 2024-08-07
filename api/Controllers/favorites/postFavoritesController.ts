7dd49a5581bb0c3424c728b98bfebb725756b258
FacundoLizardo <facundolizardo75@gmail.com>
2024-08-07 15:34:03 -0300

    Console.logs deleted

--- api/Controllers/favorites/postFavoritesController.ts
+++ api/Controllers/favorites/postFavoritesController.ts
@@ -3,10 +3,8 @@ import Favorite, { IFavorites } from "../../Models/favorites";
 const postFavoritesController = async (data: IFavorites) => {
   try {
     const newFavorite = new Favorite(data);
-    console.log({newFavorite})
     return await newFavorite.save();
   } catch (error: any) {
-    console.log({error})
     throw new Error(`Error creating favorite: ${error.message}`);
   }
 };
