06bad72cab51593e976d400f1c3dfa5005b197d0
FacundoLizardo <facundolizardo75@gmail.com>
2024-08-08 13:25:27 -0300

    backen url fix

--- client/src/utils/deleteFavorite.ts
+++ client/src/utils/deleteFavorite.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 import {INews, IResponse} from "./types.ts";
 import {toast} from "../components/ui/use-toast.ts";
 
-
+const apiUrl = import.meta.env.VITE_APP_API_URL
 const deleteFavorite = async (id: string): Promise<IResponse<INews> | undefined> => {
   try {
     return {
       success: true,
-      data: await axios.delete(`/favorites/delete?id=${id}`),
+      data: await axios.delete(`${apiUrl}/favorites/delete?id=${id}`),
     };
   } catch (e) {
     toast({
--- client/src/utils/getNews.tsx
+++ client/src/utils/getNews.tsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 import {INewsData, IResponse} from "./types.ts";
 
+const apiUrl = import.meta.env.VITE_APP_API_URL
+
 export const getNews = async (
   searchInput: string,
 ): Promise<IResponse<INewsData>> => {
   try {
-    const { data } = await axios.get(`/news?text=${searchInput}`);
+    const { data } = await axios.get(`${apiUrl}/news?text=${searchInput}`);
     return {
       success: true,
       data: data.news,
