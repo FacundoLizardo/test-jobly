f5326b51780e845e24c474e59d6a0299530dc4a6
FacundoLizardo <facundolizardo75@gmail.com>
2024-08-08 14:09:57 -0300

    Add duplicate check to favorites controller
    - Implemented a check in `postFavoritesController` to prevent adding duplicate favorites based on the `url`.
    - Added logic to verify if a favorite with the same URL already exists before saving a new entry.
    - Returned appropriate success and error messages based on the result of the check.

--- api/Controllers/favorites/postFavoritesController.ts
+++ api/Controllers/favorites/postFavoritesController.ts
@@ -2,8 +2,16 @@ import Favorite, { IFavorites } from "../../Models/favorites";
 
 const postFavoritesController = async (data: IFavorites) => {
   try {
+    const existingFavorite = await Favorite.findOne({ url: data.url });
+
+    if (existingFavorite) {
+      return { success: false, message: "Esta noticia ya está en tus favoritos" };
+    }
+
     const newFavorite = new Favorite(data);
-    return await newFavorite.save();
+    await newFavorite.save();
+
+    return { success: true, message: "Favorito agregado con éxito" };
   } catch (error: any) {
     throw new Error(`Error creating favorite: ${error.message}`);
   }
